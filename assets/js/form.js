c6f9dcf9a8924a574ef02842790c4c3bc2263db9
Felype <felype.sales@outlook.com>
2023-11-15 12:31:35 -0300

    first commit

---
+++ assets/js/form.js
@@ -0,0 +1,44 @@
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+
+    var nameValue = data.get("name");
+
+    var motivoValue = document.getElementById("motivo").value;
+
+    var messageValue = data.get("message");
+
+
+
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            Swal.fire({
+                title: "Muito obrigado!",
+                text: "Breve retornarei a mensagem! 🙏",
+                icon: "success"
+            });
+
+            form.reset();
+        } else {
+            response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                    status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
+                } else {
+                    status.innerHTML = "Oops! There was a problem submitting your form";
+                }
+            });
+        }
+    }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form";
+    });
+}
+form.addEventListener("submit", handleSubmit);
